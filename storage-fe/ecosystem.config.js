dc105799178e718cd8b48d9298808b4c0a128205
Truong.Nat <truongdq.dev@gmail.com>
2023-04-02 07:40:55 +0700

    feat: change port for FE

--- storage-fe/ecosystem.config.js
+++ storage-fe/ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       exec_mode: 'cluster',
       instances: 'max', // Or a number of instances
       script: 'node_modules/next/dist/bin/next',
-      args: 'start',
+      args: 'start -p 3001',
       env_local: {
         APP_ENV: 'local' // APP_ENV=local
       },
