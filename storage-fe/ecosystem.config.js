e11af906b2a0c7adfc0baf67ed6a573a8d84a4d2
Truong.Nat <truongdq.dev@gmail.com>
2023-04-02 07:25:49 +0700

    feat: update cmd on Dockerfile and config pm2 for fe

---
+++ storage-fe/ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      name: 'StorageFe',
+      exec_mode: 'cluster',
+      instances: 'max', // Or a number of instances
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start',
+      env_local: {
+        APP_ENV: 'local' // APP_ENV=local
+      },
+      env_development: {
+        APP_ENV: 'dev' // APP_ENV=dev
+      },
+      env_production: {
+        APP_ENV: 'prod' // APP_ENV=prod
+      }
+    }
+  ]
+}
