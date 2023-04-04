d9a994576f44fcabc198952de0ed51ced0be76fc
Truong.Nat <truongdq.dev@gmail.com>
2023-04-04 09:43:25 +0700

    feat: commit temp code

--- storage-fe/src/components/RightSide.tsx
+++ storage-fe/src/components/RightSide.tsx
@@ -1,6 +1,32 @@
-import { Card, Col, Container, Grid, Row, Text } from "@nextui-org/react";
+import {
+  Avatar,
+  Button,
+  Card,
+  Col,
+  Container,
+  Dropdown,
+  Row,
+  Spacer,
+  Text,
+  User,
+} from "@nextui-org/react";
 import React from "react";
 import { AllFilesIcon } from "@/icons/AllFilesIcon";
+import { Doughnut } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { TheeDotIcon } from "@/icons/TheeDotIcon";
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const data = {
+  labels: ["Dung lượng sử dụng"],
+  datasets: [
+    {
+      data: [30, 70],
+      backgroundColor: ["#48B4F6", "#e9e9e9"],
+      hoverBackgroundColor: ["#48B4F6", "#e9e9e9"],
+    },
+  ],
+};
 
 export default function RightSide() {
   const MockItem = ({ text }: { text: string }) => {
@@ -27,15 +53,120 @@ export default function RightSide() {
   };
 
   return (
-    <Container fluid>
-      <Grid.Container gap={2} justify="center">
-        <Grid xs={12}>
-          <MockItem text="1 of 1" />
-        </Grid>
-        <Grid xs={12}>
-          <MockItem text="1 of 1" />
-        </Grid>
-      </Grid.Container>
+    <Container
+      fluid
+      responsive={false}
+      css={{
+        p: "28px 30px",
+        h: "inherit",
+        display: "flex",
+        borderLeft: "1px solid $divider",
+        alignItems: "flex-start",
+        justifyContent: "flex-start",
+      }}
+    >
+      <Row css={{ flexDirection: "column" }}>
+        <Dropdown placement="bottom-right">
+          <Dropdown.Trigger
+            css={{ width: "100%", justifyContent: "flex-start" }}
+          >
+            <User
+              bordered
+              zoomed
+              as="button"
+              size="lg"
+              color="primary"
+              name="Tony Reichert"
+              description="@tonyreichert"
+              src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
+            />
+          </Dropdown.Trigger>
+          <Dropdown.Menu color="secondary" aria-label="Actions">
+            <Dropdown.Item key="new" command="⌘N">
+              New file
+            </Dropdown.Item>
+            <Dropdown.Item key="copy" command="⌘C">
+              Copy link
+            </Dropdown.Item>
+            <Dropdown.Item key="edit" command="⌘⇧E">
+              Edit file
+            </Dropdown.Item>
+            <Dropdown.Item withDivider key="delete" color="error" command="⌘⇧D">
+              Delete file
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+        <Row css={{ py: "30px" }}>
+          <Col>
+            <Text
+              color="$textPrimary"
+              size={"$xl"}
+              weight={"medium"}
+              css={{ lineHeight: "24px" }}
+            >
+              Storage
+            </Text>
+          </Col>
+          <Col>
+            <Button
+              css={{ h: "unset", ml: "auto" }}
+              color={"warning"}
+              auto
+              light
+              icon={
+                <TheeDotIcon
+                  fill="#84868C
+              "
+                />
+              }
+            />
+          </Col>
+        </Row>
+        <Row css={{ h: "140px", position: "relative" }}>
+          <Doughnut
+            data={data}
+            options={{
+              rotation: -90,
+              circumference: 180,
+              cutout: 50,
+              borderColor: "transparent",
+              responsive: true,
+              maintainAspectRatio: false,
+              plugins: {
+                title: {
+                  display: false,
+                },
+                legend: {
+                  display: false,
+                },
+              },
+            }}
+          />
+          <Container
+            css={{
+              position: "absolute",
+              bottom: 0,
+              left: 0,
+              right: 0,
+            }}
+            fluid
+          >
+            <Row css={{ justifyContent: "center", alignItems: "end" }} fluid>
+              <Text
+                size={"28px"}
+                color="$textPrimary"
+                css={{ fontWeight: "600", lineHeight: "33px" }}
+              >
+                70
+              </Text>
+              <Spacer x={0.2} />
+              <Text size={"14px"} color="$textMenu" css={{ fontWeight: "600" }}>
+                GB Used
+              </Text>
+            </Row>
+          </Container>
+        </Row>
+      </Row>
     </Container>
   );
 }
---
+++ storage-fe/src/icons/TheeDotIcon.tsx
@@ -0,0 +1,22 @@
+import { IconProps, getIconProps } from "@/helpers/getIconProps";
+
+export const TheeDotIcon = (props?: IconProps) => {
+  const { fill, className, height, width } = getIconProps(props);
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+      viewBox="0 0 18 4"
+      className={className}
+      {...props}
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M2 0C0.9 0 0 0.9 0 2C0 3.1 0.9 4 2 4C3.1 4 4 3.1 4 2C4 0.9 3.1 0 2 0ZM16 0C14.9 0 14 0.9 14 2C14 3.1 14.9 4 16 4C17.1 4 18 3.1 18 2C18 0.9 17.1 0 16 0ZM7 2C7 0.9 7.9 0 9 0C10.1 0 11 0.9 11 2C11 3.1 10.1 4 9 4C7.9 4 7 3.1 7 2Z"
+        fill={fill}
+      />
+    </svg>
+  );
+};
